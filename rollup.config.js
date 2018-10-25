55077de02c4f2080b70ced0cca1640758435675d
Martin Petit <martin@igloo.be>
2018-10-25 14:32:56 +0200

    Build: rollup + babel

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+
+import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+
+export default {
+  input: './src/index.js',
+  output: {
+    file: './dist/jquery.email-autocomplete.js',
+    format: 'umd',
+    name: 'jqueryEmailAutocomplete',
+    globals: {
+      'jquery': 'jQuery'
+    }
+  },
+  plugins: [
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    resolve({
+      only: [/^(?!jquery).*$/],
+      preferBuiltins: true
+    }),
+    commonjs()
+  ]
+}

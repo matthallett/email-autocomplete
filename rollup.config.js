49422b592295f608750694f7d2006c1f3f6d5b77
Martin Petit <martin@igloo.be>
2018-10-26 14:36:29 +0200

    Build: expose different build

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,39 @@
 
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
+import { uglify } from 'rollup-plugin-uglify'
 
-export default {
+const plugins = [
+  babel({
+    exclude: 'node_modules/**'
+  }),
+  commonjs()
+]
+
+const name = 'emailautocomplete'
+
+const base = {
   input: './src/index.js',
   output: {
     file: './dist/email-autocomplete.js',
-    format: 'umd',
-    name: 'emailautocomplete'
+    format: 'umd'
   },
-  plugins: [
-    babel({
-      exclude: 'node_modules/**'
-    }),
-    commonjs()
-  ]
+  plugins
 }
+
+export default [
+  Object.assign({}, base, {
+    output: [
+      { file: './dist/email-autocomplete.js', format: 'umd', exports: 'named', name },
+      { file: './dist/email-autocomplete.es.js', format: 'umd', exports: 'named', name }
+    ]
+  }),
+  Object.assign({}, base, {
+    output: [
+      { file: './dist/email-autocomplete.min.js', format: 'umd', exports: 'named', name },
+    ],
+    plugins: base.plugins.concat([
+      uglify()
+    ])
+  })
+]
